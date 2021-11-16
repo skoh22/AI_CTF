6849007abf4326759073bd8ffde5fd1035ca70d5
= <diego.rao80@gmail.com>
2021-11-16 00:17:44 -0500

    general framework incorporated, need to learn rewards and weights

--- myTeam.py
+++ myTeam.py
@@ -16,77 +16,202 @@ from captureAgents import CaptureAgent
 import random, time, util
 from game import Directions
 import game
+import os, pickle
+
 
 #################
 # Team creation #
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first = 'DummyAgent', second = 'DummyAgent'):
-  """
-  This function should return a list of two agents that will form the
-  team, initialized using firstIndex and secondIndex as their agent
-  index numbers.  isRed is True if the red team is being created, and
-  will be False if the blue team is being created.
+               first='DummyAgent', second='DummyAgent'):
+    """
+    This function should return a list of two agents that will form the
+    team, initialized using firstIndex and secondIndex as their agent
+    index numbers.  isRed is True if the red team is being created, and
+    will be False if the blue team is being created.
 
-  As a potentially helpful development aid, this function can take
-  additional string-valued keyword arguments ("first" and "second" are
-  such arguments in the case of this function), which will come from
-  the --redOpts and --blueOpts command-line arguments to capture.py.
-  For the nightly contest, however, your team will be created without
-  any extra arguments, so you should make sure that the default
-  behavior is what you want for the nightly contest.
-  """
+    As a potentially helpful development aid, this function can take
+    additional string-valued keyword arguments ("first" and "second" are
+    such arguments in the case of this function), which will come from
+    the --redOpts and --blueOpts command-line arguments to capture.py.
+    For the nightly contest, however, your team will be created without
+    any extra arguments, so you should make sure that the default
+    behavior is what you want for the nightly contest.
+    """
 
-  # The following line is an example only; feel free to change it.
-  return [eval(first)(firstIndex), eval(second)(secondIndex)]
+    # The following line is an example only; feel free to change it.
+    return [eval(first)(firstIndex), eval(second)(secondIndex)]
+
 
 ##########
 # Agents #
 ##########
 
 class DummyAgent(CaptureAgent):
-  """
-  A Dummy agent to serve as an example of the necessary agent structure.
-  You should look at baselineTeam.py for more details about how to
-  create an agent as this is the bare minimum.
-  """
-
-  def registerInitialState(self, gameState):
     """
-    This method handles the initial setup of the
-    agent to populate useful fields (such as what team
-    we're on).
-
-    A distanceCalculator instance caches the maze distances
-    between each pair of positions, so your agents can use:
-    self.distancer.getDistance(p1, p2)
-
-    IMPORTANT: This method may run for at most 15 seconds.
+    A Dummy agent to serve as an example of the necessary agent structure.
+    You should look at baselineTeam.py for more details about how to
+    create an agent as this is the bare minimum.
     """
 
-    '''
-    Make sure you do not delete the following line. If you would like to
-    use Manhattan distances instead of maze distances in order to save
-    on initialization time, please take a look at
-    CaptureAgent.registerInitialState in captureAgents.py.
-    '''
-    CaptureAgent.registerInitialState(self, gameState)
+    def registerInitialState(self, gameState):
+        """
+        This method handles the initial setup of the
+        agent to populate useful fields (such as what team
+        we're on).
 
-    '''
-    Your initialization code goes here, if you need any.
-    '''
+        A distanceCalculator instance caches the maze distances
+        between each pair of positions, so your agents can use:
+        self.distancer.getDistance(p1, p2)
+
+        IMPORTANT: This method may run for at most 15 seconds.
+        """
+
+        '''
+        Make sure you do not delete the following line. If you would like to
+        use Manhattan distances instead of maze distances in order to save
+        on initialization time, please take a look at
+        CaptureAgent.registerInitialState in captureAgents.py.
+        '''
+        CaptureAgent.registerInitialState(self, gameState)
+
+        '''
+        Your initialization code goes here, if you need any.
+        '''
+
+    def chooseAction(self, gameState):
+        """
+        Picks among actions randomly.
+        """
+        actions = gameState.getLegalActions(self.index)
+
+        '''
+        You should change this in your own agent.
+        '''
+
+        return random.choice(actions)
 
 
-  def chooseAction(self, gameState):
-    """
-    Picks among actions randomly.
-    """
-    actions = gameState.getLegalActions(self.index)
 
-    '''
-    You should change this in your own agent.
-    '''
 
-    return random.choice(actions)
+class FeatureLearningAgent(CaptureAgent):
+    def registerInitialState(self, gameState):
+        """
+        This method handles the initial setup of the
+        agent to populate useful fields (such as what team
+        we're on).
 
+        A distanceCalculator instance caches the maze distances
+        between each pair of positions, so your agents can use:
+        self.distancer.getDistance(p1, p2)
+
+        IMPORTANT: This method may run for at most 15 seconds.
+        """
+
+        '''
+        Make sure you do not delete the following line. If you would like to
+        use Manhattan distances instead of maze distances in order to save
+        on initialization time, please take a look at
+        CaptureAgent.registerInitialState in captureAgents.py.
+        '''
+        CaptureAgent.registerInitialState(self, gameState)
+
+        '''
+        Your initialization code goes here, if you need any.
+        '''
+        if os.path.exists('weights.txt'):
+            with open('weights.txt', 'r') as weightsFile:
+                self.weights = pickle.reads(weightsFile)
+        else:
+            with open('weights.txt', 'a') as weightsFile:
+                self.weights = util.Counter()
+
+        self.epsilon = 0.05
+        self.gamma = 0.8
+        self.alpha = 0.2,
+
+
+
+
+
+    def update(self, state, action, nextState, reward):
+        """
+           Should update your weights based on transition
+        """
+        # determine q values and utility for nextState
+        nextQValues = [self.getQValue(nextState, a) for a in nextState.getLegalActions(self.index)]
+        if len(nextQValues) == 0:
+            nextValue = 0
+        else:
+            nextValue = max(nextQValues)
+
+        # update
+        change = reward + (self.discount * nextValue) - self.getQValue(state, action)
+        features = self.getFeatures(state, action)
+        for f in features:
+            step = self.alpha * change * features[f]
+            self.weights[f] += step
+
+
+
+
+
+
+    def getFeatures(self, currentState, action):
+        currentPos = currentState.getAgentPosition(self.index)
+        foodPositions = filter(
+            lambda x: x,
+            self.getFood(currentState).flatten()
+        )
+        print foodPositions
+        nearestFood = min([self.distancer.getDistance(currentPos, food) for food in foodPositions])
+        score = self.getScore(currentState)
+        features = util.Counter()
+        features['food'] = nearestFood
+        features['score'] = score
+        return features
+
+
+    def getQValue(self, state, action):
+        """
+          Return Q(state,action) = w * featureVector
+          where * is the dotProduct operator
+        """
+        return self.weights * self.getFeatures(state, action)
+
+
+    def computeActionFromQValues(self, state):
+        """
+          Compute the best action to take in a state.
+        """
+        actions = state.getLegalActions(self.index)
+        if len(actions) == 0:
+            return None
+        else:
+            maxQ = -99999
+            maxActionList = []
+            for action in actions:
+                thisQ = self.getQValue(state, action)
+                if thisQ > maxQ:
+                    maxQ = thisQ
+                    maxActionList = [action]
+                elif thisQ == maxQ:
+                    maxActionList.append(action)
+            return random.choice(maxActionList)
+
+
+    def chooseAction(self, gameState):
+        """
+          Compute the action to take in the current state.  With
+          probability self.epsilon, we should take a random action and
+          take the best policy action otherwise.
+        """
+        # Pick Action
+        legalActions = gameState.getLegalActions(self.index)
+        if util.flipCoin(self.epsilon):
+            action = random.choice(legalActions)
+        else:
+            action = self.computeActionFromQValues(gameState)
+
+        return action

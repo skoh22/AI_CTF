1cbcf3588b5a5bf881b7aff974c3e5f7a6aed1b6
= <diego.rao80@gmail.com>
2021-11-15 22:15:43 -0500

    inaccurate but generally works

--- myTeam.py
+++ myTeam.py
@@ -17,76 +17,156 @@ import random, time, util
 from game import Directions
 import game
 
+
 #################
 # Team creation #
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first = 'DummyAgent', second = 'DummyAgent'):
-  """
-  This function should return a list of two agents that will form the
-  team, initialized using firstIndex and secondIndex as their agent
-  index numbers.  isRed is True if the red team is being created, and
-  will be False if the blue team is being created.
+               first='DummyAgent', second='DummyAgent'):
+    """
+    This function should return a list of two agents that will form the
+    team, initialized using firstIndex and secondIndex as their agent
+    index numbers.  isRed is True if the red team is being created, and
+    will be False if the blue team is being created.
 
-  As a potentially helpful development aid, this function can take
-  additional string-valued keyword arguments ("first" and "second" are
-  such arguments in the case of this function), which will come from
-  the --redOpts and --blueOpts command-line arguments to capture.py.
-  For the nightly contest, however, your team will be created without
-  any extra arguments, so you should make sure that the default
-  behavior is what you want for the nightly contest.
-  """
+    As a potentially helpful development aid, this function can take
+    additional string-valued keyword arguments ("first" and "second" are
+    such arguments in the case of this function), which will come from
+    the --redOpts and --blueOpts command-line arguments to capture.py.
+    For the nightly contest, however, your team will be created without
+    any extra arguments, so you should make sure that the default
+    behavior is what you want for the nightly contest.
+    """
 
-  # The following line is an example only; feel free to change it.
-  return [eval(first)(firstIndex), eval(second)(secondIndex)]
+    # The following line is an example only; feel free to change it.
+    #return [eval(first)(firstIndex), eval(second)(secondIndex)]
+    return [eval(first)(firstIndex), TrackingAgent(secondIndex)]
+
 
 ##########
 # Agents #
 ##########
+class TrackingAgent(CaptureAgent):
+    def registerInitialState(self, gameState):
+        """
+        This method handles the initial setup of the
+        agent to populate useful fields (such as what team
+        we're on).
+
+        A distanceCalculator instance caches the maze distances
+        between each pair of positions, so your agents can use:
+        self.distancer.getDistance(p1, p2)
+
+        IMPORTANT: This method may run for at most 15 seconds.
+        """
+
+        '''
+        Make sure you do not delete the following line. If you would like to
+        use Manhattan distances instead of maze distances in order to save
+        on initialization time, please take a look at
+        CaptureAgent.registerInitialState in captureAgents.py.
+        '''
+        CaptureAgent.registerInitialState(self, gameState)
+
+        '''
+        Your initialization code goes here, if you need any.
+        '''
+        self.legalPositions = []
+        for x in range(self.getFood(gameState).width):
+            for y in range(self.getFood(gameState).height):
+                if not gameState.hasWall(x, y):
+                    self.legalPositions.append((x, y))
+        self.initializeBeliefs(gameState)
+
+    def initializeBeliefs(self, gameState):
+        self.beliefs = {}
+        for agent in self.getOpponents(gameState):
+            self.beliefs[agent] = util.Counter()
+            for p in self.legalPositions:
+                if self.red:
+                    if p[0] > self.getFood(gameState).width / 2:  # initialize beliefs uniformly on blue if my team is red
+                        self.beliefs[agent][p] = 1.0
+                else:
+                    if p[0] < self.getFood(gameState).width / 2:  # initialize beliefs uniformly on red if my team is blue
+                        self.beliefs[agent][p] = 1.0
+
+    def updateBeliefs(self, prevState, currentState):
+        #prevDists = prevState.getAgentDistances()
+        currentDists = currentState.getAgentDistances()
+        for agent in self.getOpponents(currentState):
+            if self.beliefs[agent].totalCount() == 0:
+                self.beliefs[agent] = util.Counter()
+                self.beliefs[agent].incrementAll(self.legalPositions, 1.0)
+                self.beliefs[agent].normalize()
+            obs = currentState.getAgentPosition(agent)
+            if obs == None:  # if not directly observable
+                updatedBeliefs = util.Counter()
+                for p in self.legalPositions:
+                    trueDistance = util.manhattanDistance(p, currentState.getAgentPosition(self.index))
+                    updatedBeliefs[p] = currentState.getDistanceProb(trueDistance, currentDists[agent]) * self.beliefs[agent][p]
+                    updatedBeliefs.normalize()
+            else:
+                updatedBeliefs = util.Counter()
+                updatedBeliefs[obs] = 1.0
+            self.beliefs[agent] = updatedBeliefs
+
+
+    def chooseAction(self, gameState):
+        """
+        Picks among actions randomly.
+        """
+        self.updateBeliefs(self.getPreviousObservation(), self.getCurrentObservation())
+        self.displayDistributionsOverPositions([self.beliefs[agent] for agent in self.getOpponents(gameState)])
+        actions = gameState.getLegalActions(self.index)
+
+        '''
+        You should change this in your own agent.
+        '''
+
+        return random.choice(actions)
+
 
 class DummyAgent(CaptureAgent):
-  """
-  A Dummy agent to serve as an example of the necessary agent structure.
-  You should look at baselineTeam.py for more details about how to
-  create an agent as this is the bare minimum.
-  """
-
-  def registerInitialState(self, gameState):
     """
-    This method handles the initial setup of the
-    agent to populate useful fields (such as what team
-    we're on).
-
-    A distanceCalculator instance caches the maze distances
-    between each pair of positions, so your agents can use:
-    self.distancer.getDistance(p1, p2)
-
-    IMPORTANT: This method may run for at most 15 seconds.
+    A Dummy agent to serve as an example of the necessary agent structure.
+    You should look at baselineTeam.py for more details about how to
+    create an agent as this is the bare minimum.
     """
 
-    '''
-    Make sure you do not delete the following line. If you would like to
-    use Manhattan distances instead of maze distances in order to save
-    on initialization time, please take a look at
-    CaptureAgent.registerInitialState in captureAgents.py.
-    '''
-    CaptureAgent.registerInitialState(self, gameState)
+    def registerInitialState(self, gameState):
+        """
+        This method handles the initial setup of the
+        agent to populate useful fields (such as what team
+        we're on).
 
-    '''
-    Your initialization code goes here, if you need any.
-    '''
+        A distanceCalculator instance caches the maze distances
+        between each pair of positions, so your agents can use:
+        self.distancer.getDistance(p1, p2)
 
+        IMPORTANT: This method may run for at most 15 seconds.
+        """
 
-  def chooseAction(self, gameState):
-    """
-    Picks among actions randomly.
-    """
-    actions = gameState.getLegalActions(self.index)
+        '''
+        Make sure you do not delete the following line. If you would like to
+        use Manhattan distances instead of maze distances in order to save
+        on initialization time, please take a look at
+        CaptureAgent.registerInitialState in captureAgents.py.
+        '''
+        CaptureAgent.registerInitialState(self, gameState)
 
-    '''
-    You should change this in your own agent.
-    '''
+        '''
+        Your initialization code goes here, if you need any.
+        '''
 
-    return random.choice(actions)
+    def chooseAction(self, gameState):
+        """
+        Picks among actions randomly.
+        """
+        actions = gameState.getLegalActions(self.index)
 
+        '''
+        You should change this in your own agent.
+        '''
+
+        return random.choice(actions)
